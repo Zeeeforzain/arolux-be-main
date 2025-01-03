04847023269825b9cbd1a19340a374b8efdc8e09
zeeeforzain <zeeforzain@gmail.com>
2025-01-03 20:43:22 +0500

    refactored

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
-import express, { Express, Request, Response } from "express";
-import helmet from "helmet";
-import { rateLimit } from "express-rate-limit";
-import mongoose from "mongoose";
-import cors from "cors";
-import morgan from "morgan";
-import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import router from "./routes/base.routes.js";
-import "./services/crons/crons.js";
+import express, { Express, Request, Response } from 'express';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+// import router from './routes/base.routes';
+import './services/crons/crons';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -19,80 +23,68 @@ const app: Express = express();
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cors());
 
 // For logging every incoming request
 app.use(
-  morgan(function (tokens: any, req: Request, res: Response) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-    ].join(" ");
-  })
+	morgan(function (tokens: any, req: Request, res: Response) {
+		return [
+			tokens.method(req, res),
+			tokens.url(req, res),
+			tokens.status(req, res),
+			tokens.res(req, res, 'content-length'),
+			'-',
+			tokens['response-time'](req, res),
+			'ms',
+		].join(' ');
+	})
 );
-app.use("/invoices", express.static(path.join(__dirname, "invoices")));
+
+app.use('/invoices', express.static(path.join(__dirname, 'invoices')));
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 });
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter);
 
 // Database Connection
-
-// const fixieData = process.env.FIXIE_SOCKS_HOST?.split(
-// 	new RegExp('[/(:\\/@/]+')
-// );
-
-// if (fixieData) {
-// 	mongoose.connect(process.env.MONGO_URI!, {
-// 		proxyUsername: fixieData[0],
-// 		proxyPassword: fixieData[1],
-// 		proxyHost: fixieData[2],
-// 		proxyPort: +fixieData[3],
-// 	});
-// } else {
 const mongoDBURI: string = process.env.MONGO_URI!;
 await mongoose.connect(mongoDBURI);
-// }
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "App running successfully" });
+app.get('/', (req: Request, res: Response) => {
+	res.json({ message: 'App running successfully' });
 });
 
-//Routing
+// Routing
 app.use(router);
 
-app.use((req, res) => {
-  res.status(404).json({
-    success: false,
-    message: "Resource not found.",
-    data: {},
-  });
+// 404 handler
+app.use((req: Request, res: Response) => {
+	res.status(404).json({
+		success: false,
+		message: 'Resource not found.',
+		data: {},
+	});
 });
 
+// Global error handler
 app.use((err: any, req: Request, res: Response) => {
-  console.log("Exception error: ", err);
+	console.log('Exception error: ', err);
 
-  return res.status(500).json({
-    success: false,
-    message: "Error occured. Please try again later.",
-    data: {},
-  });
+	return res.status(500).json({
+		success: false,
+		message: 'An error occurred. Please try again later.',
+		data: {},
+	});
 });
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+	console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
